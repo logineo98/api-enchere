ce266fb130523c7756e370590e4c0f9c9449d555
Logineo <c.diabate@logineo.com>
2023-04-05 10:48:45 +0000

    reorganisation dans server.js

--- server.js
+++ server.js
@@ -1,21 +1,21 @@
-require("dotenv").config({ path: "./config/.env" });
-const express = require("express");
-const cors = require("cors");
-const path = require("path");
-const bodyParser = require("body-parser");
-require("./config/db");
-const app = express();
+require("dotenv").config({ path: "./config/.env" })
+const express = require("express")
+const cors = require("cors")
+const path = require("path")
+const bodyParser = require("body-parser")
+require("./config/db")
+const app = express()
 
-app.use("/api/public", express.static(path.join(__dirname, "public")));
-app.use(bodyParser.json());
-app.use(cors());
+app.use("/api/public", express.static(path.join(__dirname, "public")))
+app.use(bodyParser.json())
+app.use(cors())
 
 
 //use of routers here
 app.use("/api/user", require("./routes/user.route"))
 
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5000
 app.listen(port, () =>
     console.log(`Server listening on http://localhost:${port}`)
-);
+)
