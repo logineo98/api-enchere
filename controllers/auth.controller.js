73039b9578dd1c2ffb304769ce05a72d0c4eead9
Logineo <c.diabate@logineo.com>
2023-04-06 13:11:36 +0000

    premier commit sur la branche test

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -139,7 +139,7 @@ exports.register = (req, res) => {
                                                 })
                                                 .catch((error) => res.status(500).json({ message: error.message }))
                                         })
-                                        .catch((error) => res.status(500).json({ message: error.message }))
+                                        .catch((error) => res.status(500).json({ message: error }))
                                 })
                                 .catch((error) => res.status(500).json({ message: error.message }))
                         })
