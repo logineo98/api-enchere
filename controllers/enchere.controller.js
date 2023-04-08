4182ee81b4f0220a0b0c5693bf965d1540c0f6c2
logineo98 <c.diabate@logineo.com>
2023-04-08 22:43:38 +0000

    ajout get, get all, delete, participate, reject enchere

--- controllers/enchere.controller.js
+++ controllers/enchere.controller.js
@@ -1,33 +1,39 @@
-const { upload } = require("../middleware/middleware");
-const EnchereModel = require("../models/enchere.model");
+const { isValidObjectId } = require("mongoose")
+const { upload } = require("../middleware/middleware")
+const EnchereModel = require("../models/enchere.model")
 
 exports.create_enchere = async (req, res) => {
     try {
 
         const enchere = new EnchereModel(req.body)
         const saved_data = await enchere.save()
-        res.status(200).send({ response: saved_data, message: "Article mis en enchere avec succès mais en état d'attente; patienter le temps que nous verifions la conformité de l'article avant de le mettre en enchère. Merci" })
+        res.status(200).send({ response: saved_data, message: "Article mis en enchere avec succès mais en état d'attente patienter le temps que nous verifions la conformité de l'article avant de le mettre en enchère. Merci" })
     } catch (error) {
-        res.status(500).send({ message: error });
+        res.status(500).send({ message: error })
     }
 
 }
 
 exports.get_enchere = async (req, res) => {
     try {
+        if (!isValidObjectId(req.params.id)) return res.status(400).json({ message: "L'identifiant de l'enchère est invalide." })
 
+        const enchere = await EnchereModel.findById(req.params.id)
+        if (!enchere) return res.status(404).json({ message: "Désolé, aucune enchère correspondante n'a été trouvée." })
+        res.send({ response: enchere })
     } catch (error) {
-        res.status(500).send({ message: error.message });
+        res.status(500).send({ message: error })
     }
 
 }
 
 exports.get_all_encheres = async (req, res) => {
     try {
-
+        const encheres = await EnchereModel.find()
+        if (!encheres) throw "Une erreur est survenue au niveau du serveur lors de la recuperation des enchères."
+        res.send({ response: encheres })
     } catch (error) {
-
-        res.status(500).send({ message: error.message });
+        res.status(500).send({ message: error })
     }
 
 }
@@ -36,41 +42,94 @@ exports.update_enchere = async (req, res) => {
     try {
 
     } catch (error) {
-        res.status(500).send({ message: error.message });
+        res.status(500).send({ message: error.message })
     }
 
 }
 
 exports.delete_enchere = async (req, res) => {
     try {
+        if (!isValidObjectId(req.params.id)) return res.status(400).json({ message: "L'identifiant de l'enchère est invalide." })
 
+        const enchere = await EnchereModel.findByIdAndDelete(req.params.id)
+        if (!enchere) return res.status(404).json({ message: "Désolé, aucune enchère correspondante n'a été trouvée." })
+        res.send({ response: enchere, message: "Suppression de l'enchère effectuée avec succès." })
     } catch (error) {
-        res.status(500).send({ message: error.message });
+        res.status(500).send({ message: error })
     }
 
 }
 
 exports.participate_in_enchere = async (req, res) => {
     try {
+        const { buyerID, montant, reserve_price } = req.body
 
+        if (!isValidObjectId(req.params.id) || !isValidObjectId(buyerID)) return res.status(400).json({ message: "(enchère ID ou buyerID) est(sont) invalide(s)." })
+
+        const enchere = await EnchereModel.findById(req.params.id)
+        if (!enchere) return res.status(404).json({ message: "Désolé, aucune enchère correspondante n'a été trouvée." })
+
+        // si l'encherisseur a choisi le prix de reserve, l'enchère sera fermée
+        if (reserve_price !== false) {
+            enchere.history.push({ buyerID, reserve_price: true, montant: enchere.reserve_price })
+            enchere.enchere_status = "closed"
+
+            const enchere_after_participation = await enchere.save()
+            if (!enchere_after_participation) throw "Une erreur est survenue au niveau du serveur lors de la participation à l'enchère."
+
+            res.send({ response: enchere_after_participation })
+        } else {
+            // nous allons d'abord recuperer la derniere personne ayant participée à l'enchère afin de pouvoir faire l'incrementation des montants si le montant choisi par l'encherisseur n'est le montant de reserve
+            if (enchere.history.length !== 0) {
+                const get_last_encherisseur = enchere.history[enchere.history.length - 1]
+
+                enchere.history.push({ buyerID, montant: get_last_encherisseur.montant + montant })
+                const enchere_after_participation = await enchere.save()
+                if (!enchere_after_participation) throw "Une erreur est survenue au niveau du serveur lors de la participation à l'enchère."
+                res.send({ response: enchere_after_participation })
+            } else {
+                enchere.history.push({ buyerID, montant: enchere.started_price + montant })
+                const enchere_after_participation = await enchere.save()
+                if (!enchere_after_participation) throw "Une erreur est survenue au niveau du serveur lors de la participation à l'enchère."
+                res.send({ response: enchere_after_participation })
+            }
+        }
     } catch (error) {
-        res.status(500).send({ message: error.message });
+        res.status(500).send({ message: error.message })
     }
 
 }
 
+exports.reject_enchere = async (req, res) => {
+    try {
+        if (!isValidObjectId(req.params.id)) return res.status(400).json({ message: "L'identifiant de l'enchère est invalide." })
 
+        const { reject_motif } = req.body
 
+        const enchere = await EnchereModel.findById(req.params.id)
+        if (!enchere) return res.status(404).json({ message: "Désolé, aucune enchère correspondante n'a été trouvée." })
+
+        enchere.enchere_status = "rejected"
+        enchere.reject_motif = reject_motif
+
+        const enchere_after_reject = await enchere.save()
+        if (!enchere_after_reject) throw "Une erreur est survenue au niveau du serveur lors du rejet l'enchère."
+
+        res.send({ response: enchere_after_reject, message: "L'enchère a été rejetée." })
+    } catch (error) {
+
+    }
+}
 
 // controller function to handle file upload
 exports.upload_files = upload.array('files', 10, (req, res) => {
     try {
-        const files = req.files;
+        const files = req.files
         if (!files)
-            throw 'Please choose files';
+            throw 'Please choose files'
 
-        res.status(200).json({ response: files, message: 'Fichier uploader avec succès', });
+        res.status(200).json({ response: files, message: 'Fichier uploader avec succès', })
     } catch (error) {
-        res.status(500).send({ message: error });
+        res.status(500).send({ message: error })
     }
-});
+})
--- routes/enchere.route.js
+++ routes/enchere.route.js
@@ -1,4 +1,4 @@
-const { create_enchere, participate_in_enchere, get_enchere, get_all_encheres, update_enchere, delete_enchere, upload_files } = require('../controllers/enchere.controller')
+const { create_enchere, participate_in_enchere, get_enchere, get_all_encheres, update_enchere, delete_enchere, upload_files, get_enchere_history, reject_enchere } = require('../controllers/enchere.controller')
 const { authenticate } = require('../middleware/middleware')
 const { create_enchere_validation } = require('../utils/validations')
 
@@ -11,6 +11,7 @@ router.put("/:id/:hostID", authenticate, update_enchere)
 router.delete("/:id/:hostID", authenticate, delete_enchere)
 
 router.patch("/participate-in-enchere/:id/:hostID", authenticate, participate_in_enchere)
+router.put("/reject-enchere/:id/:hostID", authenticate, reject_enchere)
 
 router.post("/upload-file", upload_files)
 
