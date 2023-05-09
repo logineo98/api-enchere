b249b634f266864b6b5f145d2b8ce3e3a39903a9
Logineo <c.diabate@logineo.com>
2023-05-09 15:43:23 +0000

    teste vitepay

--- controllers/vitepay.controller.js
+++ controllers/vitepay.controller.js
@@ -23,11 +23,53 @@ exports.vitepay_callback = async (req, res) => {
                 // if (authenticity === our_authenticity) {
                 if (success && success == 1) {
                     if (sandbox == 1) {
-                        enchere.title = "tz nation"
-                        const enchere_after_participation = await enchere.save()
-                        if (!enchere_after_participation) return res.send({ status: 0, message: "" })
+                        // enchere.title = "tz nation"
+                        // const enchere_after_participation = await enchere.save()
+                        // if (!enchere_after_participation) return res.send({ status: 0, message: "" })
 
-                        res.send({ status: 1 })
+                        // res.send({ status: 1 })
+
+                        if (user?.tmp?.reserve_price && user?.tmp?.reserve_price === true) {
+                            enchere.history.push({ buyerID, reserve_price: true, real_montant: enchere.reserve_price, montant: enchere.reserve_price, date: new Date().getTime() })
+                            enchere.enchere_status = "closed"
+
+                            const enchere_after_participation = await enchere.save()
+                            if (!enchere_after_participation) return res.status(500).json({ status: 0, message: "Une erreur est survenue au niveau du serveur lors de la participation à l'enchère." })
+
+                            user.tmp = null
+                            const user_after_participate_encher = await user.save()
+                            if (!user_after_participate_encher) return res.status(500).json({ status: 0, message: "Une erreur est survenue au niveau du serveur lors de la reinitialisation de la variable tmp dans user" })
+
+                            res.send({ status: 1 })
+                        } else {
+                            // nous allons d'abord recuperer la derniere personne ayant participée à l'enchère afin de pouvoir faire l'incrementation des montants si le montant choisi par l'encherisseur n'est le montant de reserve
+                            if (enchere.history.length !== 0) {
+                                // recuperation du dernier encherisseur
+                                const get_last_encherisseur = enchere.history[enchere.history.length - 1]
+
+                                enchere.history.push({ buyerID, real_montant: user?.tmp?.montant, montant: get_last_encherisseur.montant + user?.tmp?.montant, date: new Date().getTime() })
+
+                                const enchere_after_participation = await enchere.save()
+                                if (!enchere_after_participation) return res.status(500).json({ status: 0, message: "Une erreur est survenue au niveau du serveur lors de la participation à l'enchère." })
+
+                                user.tmp = null
+                                const user_after_participate_encher = await user.save()
+                                if (!user_after_participate_encher) return res.status(500).json({ status: 0, message: "Une erreur est survenue au niveau du serveur lors de la reinitialisation de la variable tmp dans user" })
+
+                                res.send({ status: 1 })
+                            } else {
+                                enchere.history.push({ buyerID, real_montant: user?.tmp?.montant, montant: enchere.started_price + user?.tmp?.montant, date: new Date().getTime() })
+
+                                const enchere_after_participation = await enchere.save()
+                                if (!enchere_after_participation) return res.status(500).json({ status: 0, message: "Une erreur est survenue au niveau du serveur lors de la participation à l'enchère." })
+
+                                user.tmp = null
+                                const user_after_participate_encher = await user.save()
+                                if (!user_after_participate_encher) return res.status(500).json({ status: 0, message: "Une erreur est survenue au niveau du serveur lors de la reinitialisation de la variable tmp dans user" })
+
+                                res.send({ status: 1 })
+                            }
+                        }
                     }
                 } else if (failure && failure == 1) {
                     const enchere_updated = await EnchereModel.findByIdAndUpdate(user?.tmp?.enchereID, { title: "kougnon" }, { new: true })
